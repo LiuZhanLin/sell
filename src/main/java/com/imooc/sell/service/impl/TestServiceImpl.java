474a7b4942a01d44f8dbef382c9af30c394c581a
“LiuZhanLin”  <liuzhanlin900925@163.com>
2019-01-29 14:01:04 +0800

    测试服务类修改

--- src/main/java/com/imooc/sell/service/impl/TestServiceImpl.java
+++ src/main/java/com/imooc/sell/service/impl/TestServiceImpl.java
@@ -24,7 +24,7 @@ public class TestServiceImpl implements TestService {
     @Override
     public String getAccess(String role) {
         if(!StringUtils.isEmpty(role)&&role.equals("admin")){
-            return "001；002";
+            return "001；002；003";
         }
         return "error role";
     }
